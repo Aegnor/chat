6e3ac1917a4c7a5ac5e49f6ebca57d916382bebd
Sbr <12alexman@gmail.com>
2021-03-11 18:52:02 +0200

    axios - add base API url

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,10 @@
 import {createApp} from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+
+axios.defaults.baseURL = 'http://localhost:3030'
 
 createApp(App).use(store).use(router).mount('#app')

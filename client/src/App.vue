9dd5265b7f03ad18589ba919fb241e7a2a917460
Sbr <12alexman@gmail.com>
2021-03-11 18:23:36 +0200

    connect socket.io, import base styles

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,17 @@
 <template>
     <router-view/>
 </template>
+
+<script>
+import { io } from 'socket.io-client'
+
+export default {
+    mounted() {
+        io('http://localhost:3030')
+    }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/base.scss';
+</style>

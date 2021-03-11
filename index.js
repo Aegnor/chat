0dc7bcdc7d64ea227cea798182a730b8add609b6
Sbr <12alexman@gmail.com>
2021-03-11 15:54:43 +0200

    add cors helmet, connect socket.io, connect mongoose, create server

---
+++ index.js
@@ -0,0 +1,40 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const env_config = require('./config/index')
+
+const http = require('http').createServer(app)
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'http://localhost:8080'
+    }
+})
+
+const PORT = process.env.PORT ?? 3030
+
+app.use(require('helmet')())
+app.use(require('cors')())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+async function start() {
+    try {
+        await mongoose.connect(env_config.DATABASE_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        })
+
+        http.listen(PORT, () => console.log(`server listening on http://localhost:${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start();

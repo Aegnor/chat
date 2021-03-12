391c8d07e02c696fd075e68970ca04a8e96c05a9
Sbr <12alexman@gmail.com>
2021-03-12 17:19:48 +0200

    add page not found component

---
+++ client/src/views/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <p class="error-404">404 error page not found</p>
+</template>
+
+<script>
+export default {
+    name: 'PageNotFound'
+}
+</script>
+
+<style lang="scss">
+.error-404 {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2.4rem;
+}
+</style>

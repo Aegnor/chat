b59dcd321b8b2f9992f0d27fef03edc08d5380da
Sbr <12alexman@gmail.com>
2021-03-14 15:26:05 +0200

    add Navbar and Notification component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-    <router-link to="/">Go to Chat</router-link>
-    <br>
-    <router-link to="/auth">Auth</router-link>
-    <router-view/>
+    <Navbar/>
+    <main id="main-content">
+        <Notification/>
+        <router-view/>
+    </main>
 </template>
 
 <script>
 import {io} from 'socket.io-client'
+import Notification from '@/components/Notification'
+import Navbar from '@/components/Navbar'
 
 export default {
+    components: {
+        Notification,
+        Navbar
+    },
     mounted() {
         io('http://localhost:3030')
     }

81827f45af15fbbdf6fb8e78a6134c2ab4ecff3f
Sbr <12alexman@gmail.com>
2021-03-14 15:26:41 +0200

    create notification component

---
+++ client/src/components/Notification.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="notification container-small">
+        <p>hello i am Notification</p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Notification'
+}
+</script>
+
+<style lang="scss">
+.notification {
+    position: fixed;
+    top: 40px;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+    background-color: #57a254;
+}
+</style>

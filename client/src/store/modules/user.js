03bf6a86142329bbd6bf8f41ae3281cb6847fb92
Sbr <12alexman@gmail.com>
2021-03-16 17:48:43 +0200

    remove localStorage

--- client/src/store/modules/user.js
+++ client/src/store/modules/user.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default {
     state: {
-        user: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null
+        user: null
     },
     getters: {
         user: state => state.user
@@ -12,7 +12,6 @@ export default {
             state.user = user
         },
         logout(state) {
-            localStorage.removeItem('user')
             state.user = null
         }
     },
@@ -22,8 +21,6 @@ export default {
                 const user = await axios.post('api/v1/auth', formData)
                 const {token, msg} = user.data
                 commit('setUser', token)
-                // @TODO: refactor local storage
-                localStorage.setItem('user', JSON.stringify(token))
                 if (msg) {
                     dispatch('addNotification', {
                         data: msg,

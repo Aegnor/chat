1f80404f36b8495ca776b2b5e96deae4a84ab9c8
Sbr <12alexman@gmail.com>
2021-03-16 21:35:47 +0200

    create ChatSidebar

---
+++ client/src/components/chat/ChatSidebar.vue
@@ -0,0 +1,42 @@
+<template>
+    <aside class="chat-sidebar" v-if="users.length">
+        <h2 class="visually-hidden">List of online users</h2>
+        <ul class="users-list">
+            <li v-for="user in users" :key="user.name"><span>{{user.name}}</span> are online</li>
+        </ul>
+    </aside>
+</template>
+
+<script>
+export default {
+    name: 'ChatSidebar',
+    data() {
+        return {
+            users: []
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.chat-sidebar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 25%;
+    max-width: 300px;
+    padding: 10px;
+
+    @include responsive(1240) {
+        position: static;
+        max-width: 850px;
+        margin: 0 auto;
+        width: auto;
+    }
+
+    li {
+        font-size: 1.6rem;
+        margin-bottom: 10px;
+    }
+}
+</style>

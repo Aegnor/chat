92c8504dc43c375f6c9f8e3aca32c8128731bc23
Sbr <12alexman@gmail.com>
2021-03-17 20:57:34 +0200

    remove socket io connection from whole App

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,15 +6,11 @@
 </template>
 
 <script>
-import {io} from 'socket.io-client'
 import Notification from '@/components/Notification'
 
 export default {
     components: {
         Notification
-    },
-    mounted() {
-        io('http://localhost:3030')
     }
 }
 </script>

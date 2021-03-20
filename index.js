af2023501c65427066c7919d1055f00e9c46d698
Sbr <12alexman@gmail.com>
2021-03-20 18:20:31 +0200

    add csrfProtection

--- index.js
+++ index.js
@@ -13,19 +13,33 @@ const io = require('socket.io')(http, {
 
 const PORT = process.env.PORT ?? 3030
 
+const csrfProtection = require('csurf')({
+    cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 3600 // 1-hour
+    }
+})
+
 app.use(require('helmet')())
 app.use(require('cors')())
+app.use(require('cookie-parser')())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use('/api/v1/auth', require('./routes/auth'))
+app.use('/api/v1/auth', csrfProtection, require('./routes/auth'))
+
+// Create csrfToken
+app.get('/form', csrfProtection, (req, res) => {
+    res.send({ csrfToken: req.csrfToken()})
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected')
-})
+});
 
-async function start() {
+(async () => {
     try {
         await mongoose.connect(env_config.DATABASE_URI, {
             useNewUrlParser: true,
@@ -37,6 +51,4 @@ async function start() {
     } catch (e) {
         console.log(e)
     }
-}
-
-start()
+})()

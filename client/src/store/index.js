8306989b7e4f8bd63d178992bc8a47870c1cc13e
Sbr <12alexman@gmail.com>
2021-03-21 13:42:10 +0200

    rename message.js to socket.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,12 @@
 import {createStore} from 'vuex'
 import userModule from './modules/user'
 import notificationModule from './modules/notification'
-import messagesModule from './modules/messages'
+import socketModule from './modules/socket'
 
 export default createStore({
     modules: [
         userModule,
         notificationModule,
-        messagesModule
+        socketModule
     ]
 })

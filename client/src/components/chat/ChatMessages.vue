1c32b4ddd9afd785773f9c9764c1bfb0f467762e
Sanjarbuck <orionblankoff@gmail.com>
2021-03-28 15:12:54 +0300

    improve markup

--- client/src/components/chat/ChatMessages.vue
+++ client/src/components/chat/ChatMessages.vue
@@ -7,11 +7,15 @@
                 v-for="message in messages"
                 :key="message.id"
             >
-                <p class="user">{{message.user}}:</p>
+                <div class="user">
+                    <div class="user-control" v-if="user.role === 'admin' && !message.id === user._id">
+                        <button class="user-control-button" aria-label="Mute User">Mute</button>
+                        <button class="user-control-button" aria-label="Ban User">Ban</button>
+                    </div>
+                    <span>{{message.user}}:</span>
+                </div>
                 <p class="user-message"
-                   :class="{
-                            'user-message-owner': message.id
-                        }"
+                   :class="{'user-message-owner': message.id === user._id}"
                 >{{message.text}}</p>
             </li>
         </ul>
@@ -24,7 +28,8 @@ export default {
     name: 'ChatMessages',
     data() {
         return {
-            messages: this.$store.getters.messages
+            messages: this.$store.getters.messages,
+            user: this.$store.getters.user
         }
     }
 }
@@ -42,6 +47,13 @@ export default {
         &-owner {
             background-color: #fff;
             color: #000;
+        }
+    }
+
+    &-control {
+        margin-bottom: 5px;
+        &-button {
+            margin-right: 5px;
         }
     }
 }
